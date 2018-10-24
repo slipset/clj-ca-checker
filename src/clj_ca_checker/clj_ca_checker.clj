a0ea916eb606c048227f03f2c7d02ef851075f00
Erik Assum <erik@assum.net>
2018-10-24 16:00:40 +0200

    Very crude first version

--- src/clj_ca_checker/clj_ca_checker.clj
+++ src/clj_ca_checker/clj_ca_checker.clj
@@ -1,7 +1,13 @@
 (ns clj-ca-checker.clj-ca-checker
   (:gen-class))
 
+(defn find-user [user]
+  (->> (slurp "https://www.clojure.org/community/contributors")
+       (re-find (re-pattern user))))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [github-user]
+  (if (find-user github-user)
+    (System/exit 0)
+    (System/exit 1)))
