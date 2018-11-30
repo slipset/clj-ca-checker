814c8ad74cf4656e1311afaef61e0dde6068ff44
Erik Assum <erik.assum@ardoq.com>
2018-11-30 16:41:25 +0100

    [Fix #3] Get contributors from github
    - this is a csv file. Removes the need for hickory

--- src/clj_ca_checker/clj_ca_checker.clj
+++ src/clj_ca_checker/clj_ca_checker.clj
@@ -1,49 +1,25 @@
 (ns clj-ca-checker.clj-ca-checker
-  (:require [hickory.core :as hic]
-            [hickory.select :as s])
+  (:require [clojure.string :as str])
   (:gen-class))
 
-(defn user-table [hickory]
-  (s/select (s/descendant (s/tag :html)
-                          (s/tag :body)
-                          (s/and (s/tag :div)
-                                 (s/nth-child 3))
-                          (s/tag :div)
-                          (s/and (s/tag :div)
-                                 (s/nth-child 2))
-                          (s/and (s/tag :div)
-                                 (s/nth-child 3))
-                          (s/tag :div)
-                          (s/tag :table)
-                          (s/tag :tbody)) hickory))
+(def contributors-source
+  "https://raw.githubusercontent.com/clojure/clojure-site/master/content/community/contributors.csv")
+(defn fetch-contributors []
+  (->> contributors-source
+       slurp
+       str/split-lines
+       (map #(str/split % #","))))
 
-(defn user [tr]
-  (let [user-name (get-in tr [:content 3 :content 0 :content 0])
-        name (get-in tr [:content 1 :content 0 :content 0])]
-    [name user-name]))
-
-(defn users [html]
-  (->> html
-       hic/parse
-       hic/as-hickory
-       user-table
-       first
-       :content
-       (remove string?)
-       (map user)))
-
-(defn find-user [user]
-  (->> (slurp "https://www.clojure.org/community/contributors")
-       users
+(defn contributor? [contributors github-user]
+  (->> contributors
        (map second)
-       (filter (partial = user))
+       (filter (partial = github-user))
        first))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [github-user]
   (if (or (= github-user "richhickey") ;; you never know
-          (find-user github-user))
+          (contributor? (fetch-contributors) github-user))
     (System/exit 0)
     (System/exit 1)))
 
